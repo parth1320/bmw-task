8c57b20c8e05cd4f203a0c3fd541f086af37e6be
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-04-11 18:50:13 +0200

    Add files via upload

---
+++ server/database/sequelize.config.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const sequelize = new Sequelize("cell_test_data", "root", "", {
+  host: "localhost",
+  dialect: "mysql", // Choose your database dialect
+});
+
+module.exports = sequelize;
