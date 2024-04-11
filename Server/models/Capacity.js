3a7d6d7c5a837b023ded06280621f7e15a3dccbf
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-04-11 18:53:42 +0200

    Delete Server directory

--- Server/models/Capacity.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../database/sequelize.config");
-
-const Capacity = sequelize.define("Capacity", {
-  cycle_number: { type: DataTypes.INTEGER, allowNull: false },
-  capacity: { type: DataTypes.FLOAT, allowNull: false },
-});
-
-module.exports = Capacity;
